933253750eafb9cdb64a86ce3e4c43ba50bef4fa
MarcanteVitor <marcanteluizvitor@gmail.com>
2025-02-17 16:31:05 -0300

    ADicionei o raposo invertido

--- core/src/com/mygdx/game/Assets.java
+++ core/src/com/mygdx/game/Assets.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 public class Assets {
     public static final AssetManager manager = new AssetManager();
     public static final String RAPOSO_TEXTURE = "raposo.png";
+    public static final String RAPOSO_INVERTIDO_TEXTURE = "raposo_invertido.png";
     public static final String BACKGROUND_TEXTURE = "background.jpg";
     public static final String GALINHA_TEXTURE = "galinha.png";
     public static final String OVO_TEXTURE = "ovo.png";
@@ -16,6 +17,7 @@ public class Assets {
 
     public static void load() {
         manager.load(RAPOSO_TEXTURE, Texture.class);
+        manager.load(RAPOSO_INVERTIDO_TEXTURE, Texture.class);
         manager.load(BACKGROUND_TEXTURE, Texture.class);
         manager.load(GALINHA_TEXTURE, Texture.class);
         manager.load(OVO_TEXTURE, Texture.class);
